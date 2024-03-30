e5a97c225f886226c65994de35719f31ae4db9df
Jose Alejandro Moreno Mesa <43299594+joseamorenom@users.noreply.github.com>
2024-03-30 04:49:46 +0000

    corrección lógica de la segmentación de los números

--- display_7seg.c
+++ display_7seg.c
@@ -1,5 +1,5 @@
 #include "display_7seg.h"
-#include "pico/stdlib.h" //Librería de las Raspberry
+#include "pico/stdlib.h" //Librería de las Raspberry Pi Pico
 
 // Conjunto de los segmentos
 const int segment_pins[] = {SEGMENT_A_PIN, SEGMENT_B_PIN, SEGMENT_C_PIN, SEGMENT_D_PIN, SEGMENT_E_PIN, SEGMENT_F_PIN, SEGMENT_G_PIN, SEGMENT_DP_PIN};
@@ -13,18 +13,15 @@ void display_7seg_init() {
     }
 }
 
-//Mostrar un número en los 4 dígitos del display de 7 segmentos
-void display_7seg_show_number(int number) {
+//Mostrar un número completo en los 4 dígitos del display de 7 segmentos
+void display_7seg_show_number_complete(int number_ms) {
     int digits[4]; // Array para almacenar los dígitos de "number" en cada posición
 
     // Extraer cada dígito de number y almacenarlo en el array digits
-    digits[0] = number % 10;
-    number /= 10;
-    digits[1] = number % 10;
-    number /= 10;
-    digits[2] = number % 10;
-    number /= 10;
-    digits[3] = number % 10;
+    digits[0] = (number_ms / 1000) % 10; // Dígito de los segundos más significativo
+    digits[1] = (number_ms / 100) % 10;  // Dígito de los segundos menos significativo
+    digits[2] = (number_ms / 10) % 10;   // Dígito de las milésimas de segundo más significativo
+    digits[3] = number_ms % 10;          // Dígito de las milésimas de segundo menos significativo
 
     // Mostrar los dígitos en los 4 displays de 7 segmentos
     for (int digit_index = 0; digit_index < 4; digit_index++) {
@@ -33,69 +30,72 @@ void display_7seg_show_number(int number) {
             gpio_put(segment_pins[i], 0);
         }
 
-        // Mostrar el dígito correspondiente en el display actual (va de a uno)
+        // Mostrar el dígito correspondiente en el display actual
         switch (digits[digit_index]) {
             case 0:
                 for (int i = 0; i < 7; i++) {
-                    gpio_put(segment_pins[i], DIGIT_0 & (1 << i));
+                    gpio_put(segment_pins[i], DIGIT_0 & (1 << (6 - i)));
                 }
                 break;
             case 1:
                 for (int i = 0; i < 7; i++) {
-                    gpio_put(segment_pins[i], DIGIT_1 & (1 << i));
+                    gpio_put(segment_pins[i], DIGIT_1 & (1 << (6 - i)));
                 }
                 break;
             case 2:
                 for (int i = 0; i < 7; i++) {
-                    gpio_put(segment_pins[i], DIGIT_2 & (1 << i));
+                    gpio_put(segment_pins[i], DIGIT_2 & (1 << (6 - i)));
                 }
                 break;
             case 3:
-                for (int i = 0; i < 7; i++)  {
-                    gpio_put(segment_pins[i], DIGIT_3 & (1 << i));
+                for (int i = 0; i < 7; i++) {
+                    gpio_put(segment_pins[i], DIGIT_3 & (1 << (6 - i)));
                 }
                 break;
             case 4:
                 for (int i = 0; i < 7; i++) {
-                    gpio_put(segment_pins[i], DIGIT_4 & (1 << i));
-                }   
+                    gpio_put(segment_pins[i], DIGIT_4 & (1 << (6 - i)));
+                }
                 break;
             case 5:
-                for (int i = 0; i < 7; i++) {   
-                    gpio_put(segment_pins[i], DIGIT_5 & (1 << i));
-                }   
+                for (int i = 0; i < 7; i++) {
+                    gpio_put(segment_pins[i], DIGIT_5 & (1 << (6 - i)));
+                }
                 break;
             case 6:
                 for (int i = 0; i < 7; i++) {
-                    gpio_put(segment_pins[i], DIGIT_6 & (1 << i));
-                }   
+                    gpio_put(segment_pins[i], DIGIT_6 & (1 << (6 - i)));
+                }
                 break;
             case 7:
                 for (int i = 0; i < 7; i++) {
-                    gpio_put(segment_pins[i], DIGIT_7 & (1 << i));
-                }   
+                    gpio_put(segment_pins[i], DIGIT_7 & (1 << (6 - i)));
+                }
                 break;
             case 8:
                 for (int i = 0; i < 7; i++) {
-                    gpio_put(segment_pins[i], DIGIT_8 & (1 << i));
-                }   
+                    gpio_put(segment_pins[i], DIGIT_8 & (1 << (6 - i)));
+                }
                 break;
             case 9:
                 for (int i = 0; i < 7; i++) {
-                    gpio_put(segment_pins[i], DIGIT_9 & (1 << i));
-                }   
-                break;
-            
-            default:
-                //Todo apagado
-                for (int i = 0; i < 7; i++) {
-                    gpio_put(segment_pins[i], DIGIT_OFF & (1 << i));
+                    gpio_put(segment_pins[i], DIGIT_9 & (1 << (6 - i)));
                 }
                 break;
-            
+            default:
+                // Todo apagado
+                for (int i = 0; i < 7; i++) {
+                    gpio_put(segment_pins[i], DIGIT_OFF & (1 << (6 - i)));
+                }
+                break;
         }
 
-        // Reatrdo para que los dígitos se muestren uno a uno
-        sleep_ms(1);
+        // Enciende el punto decimal en el segundo display
+        if (digit_index == 1) {
+            gpio_put(segment_pins[SEGMENT_DP_PIN], 1);
+        }
+
+        // Retardo para que los dígitos se muestren uno a uno
+        sleep_ms(4);
     }
 }
